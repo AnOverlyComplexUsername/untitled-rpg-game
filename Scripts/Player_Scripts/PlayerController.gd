8beaff4f756091d5fc95ab851c15bbae1ee35b15
AnOverlyComplexUsername <AnOverlyComplexEmail@gmail.com>
2025-10-14 23:22:51 -0500

    added test enviroment & basic movement script

--- Scripts/Player_Scripts/PlayerController.gd
+++ Scripts/Player_Scripts/PlayerController.gd
@@ -1,6 +1,17 @@
-extends Node
+extends CharacterBody2D
 class_name Player 
 
 #todo: Finish basic top down 2d movement; thinking of using composition for this (?)
 
-@export var speed : float = 200.0
+@export var speed : float = 200.0 
+
+@export var canMove : bool = true #used to toggle/enable movement
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	if canMove:
+		get_input()
+		move_and_slide()
