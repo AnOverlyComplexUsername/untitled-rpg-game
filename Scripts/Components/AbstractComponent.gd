2f48239abcbfccd274afd3e0ccd45099be391632
AnOverlyComplexUsername <AnOverlyComplexEmail@gmail.com>
2025-10-14 22:17:56 -0500

    Added abstract component

---
+++ Scripts/Components/AbstractComponent.gd
@@ -0,0 +1,9 @@
+@abstract 
+class_name AbstractComponent
+extends Node
+#abstract component; has functions for enabling/disabling itself
+func enable():
+	self.enable()
+	
+func disable():
+	self.disable()
