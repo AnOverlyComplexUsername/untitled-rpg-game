b7889bff38912bd1cc872735cdc0cb146d87781c
AnOverlyComplexUsername <AnOverlyComplexEmail@gmail.com>
2025-10-20 17:44:22 -0500

    Player can now select limb to target

--- Scripts/Components/Limbs/Limb.gd
+++ Scripts/Components/Limbs/Limb.gd
@@ -3,14 +3,31 @@ extends AbstractLimbEntity
 ##A basic limb implementation of AbstractLimb
 class_name Limb
 
+var selected = false
+
+
 func on_death() -> void:
 	self.queue_free()
 
 func mouse_hover() -> void:
 	print('buh')
 	if targettable:
-		self.get_material().set_shader_parameter("color", Color("ffffffff"))
+		if selected: 
+			self.get_material().set_shader_parameter("color", Color("ffffffff"))
+		else:
+			self.get_material().set_shader_parameter("color", Color("ffffff9a"))
+		Global.battle_manager.set_hovered_limb(self)
 		
 func mouse_leave():
 	print("uh")
+	Global.battle_manager.set_hovered_limb(null)
 	self.get_material().set_shader_parameter("color", Color("ffffff00"))
+
+func select():
+	selected = true
+	self.get_material().set_shader_parameter("color", Color("ffffffff"))
+
+
+func deselect():
+	selected = false
+	print('deselected')
--- Scripts/Game_Management/BattleManager.gd
+++ Scripts/Game_Management/BattleManager.gd
@@ -4,5 +4,25 @@ extends Node2D
 ## actions during battle
 class_name BattleManager
 
+var targettedEnemyLimb : Limb = null
+var hoveredEnemyLimb : Limb = null
+
 func _ready():
 	Global.battle_manager = self
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT :
+			print("Left mouse button")
+			if hoveredEnemyLimb != null:
+				set_targetted_limb(hoveredEnemyLimb)
+				targettedEnemyLimb.select()
+			
+func set_targetted_limb(limb : Limb):
+	if targettedEnemyLimb != null and targettedEnemyLimb != limb:
+		targettedEnemyLimb.deselect()
+	targettedEnemyLimb = limb
+	 
+func set_hovered_limb(limb : Limb):
+	hoveredEnemyLimb = limb
