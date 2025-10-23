3a7e695ea8ab1af81003e0bfbcffee04dbab956c
AnOverlyComplexUsername <AnOverlyComplexEmail@gmail.com>
2025-10-22 23:10:22 -0500

    Damage Numbers implimented

--- Scripts/Components/Limbs/AttackingLimb.gd
+++ Scripts/Components/Limbs/AttackingLimb.gd
@@ -6,8 +6,12 @@ class_name AttackingLimb
 @export var damage : int ##Damage that the limb does when attacking
 @export var hitChance : float ##Chance it has to hit/miss
 
-
 ##Attacks targetted limb
 func attack(limb : AbstractLimbEntity) -> void: 
 	limb.healthComp.damage(damage) 
+	var popup = damageNumPopup.new_popup(damage, 
+	Vector2(limb.clickArea.get_child(0).global_position)
+	)	
+	get_tree().get_root().add_child(popup)
+	#limb.position
 	##TODO: implement algorithm for determining hit success
